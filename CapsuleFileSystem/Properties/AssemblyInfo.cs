d65223d019d0744708dd9bce1686b583bae1bb7e
Amadeusz Sadowski <amis.tau.92@gmail.com>
2015-01-19 18:19:57 +0100

    renaming

--- CapsuleFileSystem/Properties/AssemblyInfo.cs
+++ CapsuleFileSystem/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("CapsuleFileSystem")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Amadeusz Sadowski")]
+[assembly: AssemblyProduct("CapsuleFileSystem")]
+[assembly: AssemblyCopyright("Copyright © Amadeusz Sadowski 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
